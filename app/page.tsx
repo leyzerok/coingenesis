2daccd4e422f13b90bfbe1b3b0cc70b68ae855d4
dcrescimbeni <crescimbenidino@gmail.com>
2024-08-03 17:19:58 -0300

    x

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { LaunchpadTable } from "./LaunchpadTable";
 import { PrismaClient, ProjectStatus } from "@prisma/client";
 
-async function getProjects(status: ProjectStatus) {
+async function getProjects(status: ProjectStatus, isActive: boolean) {
   const prisma = new PrismaClient();
-  const projects = await prisma.project.findMany({ where: { status } });
+  const projects = await prisma.project.findMany({
+    where: { status, isActive },
+  });
 
   const mockProjects = projects.map((project) => {
     return {
@@ -20,8 +22,8 @@ async function getProjects(status: ProjectStatus) {
 }
 
 export default async function Home() {
-  const deployedProjects = await getProjects("DEPLOYED");
-  const pendingProjects = await getProjects("PENDING");
+  const activeProjects = await getProjects("DEPLOYED", true);
+  const endedProjects = await getProjects("DEPLOYED", false);
 
   return (
     <div className="border flex flex-col items-center">
@@ -29,14 +31,14 @@ export default async function Home() {
         <h2 className="text-3xl text-center py-14 font-bold">
           Active Launchpads
         </h2>
-        <LaunchpadTable items={deployedProjects} />
+        <LaunchpadTable items={activeProjects} />
       </div>
 
       <div className="mt-11">
         <h2 className="text-3xl text-center py-14 font-bold">
           Ended Launchpads
         </h2>
-        <LaunchpadTable items={pendingProjects} showActions />
+        <LaunchpadTable items={endedProjects} />
       </div>
     </div>
   );
