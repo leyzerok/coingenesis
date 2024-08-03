a69ec994992242ca25969f67f5a2375a489121b1
paul.sizon <paul.sizon@outlook.com>
2024-08-03 15:22:07 -0300

    community page

---
+++ app/community/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Image from 'next/image';
+import argentinaeth from '/public/argentinaeth.svg';
+import x from '/public/x.svg';
+import scroll from '/public/scroll.svg';
+import question from '/public/question.svg';
+import banana from '/public/banana.svg';
+
+const Community: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#dcdee4] p-4">
+            <h1 className="text-4xl font-bold mb-16 mt-8">Community</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-16 gap-x-0 max-w-5xl">
+                <div className="text-center lg:text-left order-2 lg:order-1">
+                    <h1 className="text-2xl font-bold mb-4">Certified Deployer Attestation</h1>
+                    <p className="text-lg mb-8">
+                        Certified Deployer Attestation is an exclusive badge awarded to individuals or corporates who have successfully launched more than three tokens on the CoinGenesis Launchpad Platform. This certification signifies trust, experience, and reliability in the token deployment process, making these deployers more attractive to investors and partners. The certification not only recognizes their proven track record but also enhances their credibility within the community.
+                    </p>
+                </div>
+                <div className="flex justify-center lg:justify-end order-1 lg:order-2">
+                    <div className="flex space-x-2">
+                        <Image src={argentinaeth} alt="Ethereum Argentina" height={300} width={300} />
+                        <Image src={x} alt="X" height={20} width={20} />
+                        <Image src={scroll} alt="Scroll" height={300} width={250} />
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center lg:items-start order-3 lg:order-3 space-y-2 ml-20 mt-20">
+                    <Image src={question} alt="Question" height={96} width={96} />
+                    <Image src={banana} alt="Banana" height={96} width={96} />
+                </div>
+                <div className="text-center lg:text-left order-4 lg:order-4">
+                    <h1 className="text-2xl font-bold mb-4">$BANANA is Coming Soon to CoinGenesis Launchpad</h1>
+                    <p className="text-lg mb-8">
+                        Get ready for a fruity revolution! $BANANA, the most anticipated memecoin of the season, is set to launch on CoinGenesis Launchpad.  This vibrant and fun-filled token aims to bring a fresh twist on Scroll Mainnet. $BANANA promises to deliver unique features and community-driven innovations. Stay tuned for more updates and prepare to peel away the layers of this exciting new project!
+                    </p>
+                </div>
+            </div>
+            <button className="bg-transparent border border-black text-black py-4 px-8 text-lg rounded-full hover:bg-black hover:text-white transition mt-8 mb-8">
+                Stay Tuned for Updates
+            </button>
+        </div>
+    );
+};
+
+export default Community;
