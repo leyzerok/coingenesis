a42c4b4650827f824fadeaf7e17fcb8e8c7a3be0
paul.sizon <paul.sizon@outlook.com>
2024-08-02 21:41:42 -0300

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
         <body className={grotesk.className}>
           <>
             <Header />
-            <main className="flex-grow">{children}</main>
-          <Footer />          
+            {children}
+            <Footer />
           </>
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import { Web3Provider } from "./Web3Provider";
-import { ConnectKitButton } from "connectkit";
-import Header from "./Header";
-
 export default function Home() {
   return <div> hello world</div>;
 }
